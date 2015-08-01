5bbcb6b74cc6ed4ed7f2f687d9db3acbf2b23b38
Robin Malhotra <robinmalhotra@robins-imac.local>
2015-08-02 00:57:44 +0530

    Initial Commit

---
+++ DynamicTrayDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicTrayDemo
+//
+//  Created by Robin Malhotra on 02/08/15.
+//  Copyright © 2015 Robin Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
